e8dbc8eb31839051e737373b7977aede3f9ffbb8
Styn van de Haterd <chaosnova2011@hotmail.com>
2023-12-11 11:42:55 +0100

    ConvertToDecimalIfNumber

--- NovaBasicLanguage/Language/STL/Functions/ComparisonFunction.cs
+++ NovaBasicLanguage/Language/STL/Functions/ComparisonFunction.cs
@@ -52,6 +52,8 @@ public class ComparisonFunction : IStlFunction
 
     private static object? ExecuteOperand(string op, dynamic lhs, dynamic rhs)
     {
+        lhs = ConvertToDecimalIfNumber(lhs);
+        rhs = ConvertToDecimalIfNumber(rhs);
         return op switch
         {
             Tokens.PLUS => Operations.Add(lhs, rhs),
@@ -97,5 +99,42 @@ public class ComparisonFunction : IStlFunction
             lhsArray[i] = ExecuteOperand(op, lhsArray[i] as dynamic, rhsArray[i] as dynamic);
         }
         return lhsArray;
+    }
+
+    public static dynamic? ConvertToDecimalIfNumber(dynamic input)
+    {
+        if (input is null)
+        {
+            return null;
+        }
+
+        if (!IsNumber(input))
+        {
+            return input;
+        }
+
+        try
+        {
+            return Convert.ToDecimal(input);
+        }
+        catch (Exception ex) when (ex is FormatException || ex is InvalidCastException || ex is OverflowException)
+        {
+            throw new ArgumentException("Input must be a numeric type", nameof(input));
+        }
+    }
+
+    public static bool IsNumber(object value)
+    {
+        return value is sbyte
+            || value is byte
+            || value is short
+            || value is ushort
+            || value is int
+            || value is uint
+            || value is long
+            || value is ulong
+            || value is float
+            || value is double
+            || value is decimal;
     }
 }
